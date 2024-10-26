840c48d83b7129e4cbb3900bc1f89186e33113f6
wesleysaraivaa <wesley-saraiva@outlook.com>
2024-10-25 22:46:40 -0300

    instalacao do react e tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Verifica os arquivos JS e TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
