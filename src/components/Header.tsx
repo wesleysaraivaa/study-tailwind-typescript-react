42dafeab3e883de21c0350ddb33dbe3c5db2bf55
wesleysaraivaa <wesley-saraiva@outlook.com>
2024-10-25 23:16:02 -0300

    adicionado componente HEADER

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from './../assets/LogoUni.png'
+
+const Header: React.FC = () =>{ 
+    return(
+        <header className ="flex justify-between items-center bg-blue-600 text-white p-4 font-bold shadow-md" >
+            
+            <div className="flex items-center gap-4">
+                <img src={Logo} alt="logo" width={60} height={60}/>
+                <h1 className="text-2xl"> UniwersoTech </h1>
+            </div>
+
+            <div>
+                <ul className="flex gap-5">
+                    <li>Home</li>
+                    <li>Contato</li>
+                    <li>Sobre</li>
+                </ul>
+            </div>
+
+        </header>    
+    );
+}
+
+export default Header;
