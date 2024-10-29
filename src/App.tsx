5e4f9cba11cd98057d50fb36a813001d36fa04e7
wesleysaraivaa <wesley-saraiva@outlook.com>
2024-10-29 20:45:43 -0300

    criacao do component footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Header />
       <Sidebar />
       <Main />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+    return(
+        <footer className="bg-gray-800 text-white  p-4 mt-4">
+            <p className="text-center"> ©2024 Meu Layout de Estudos </p>
+        </footer>
+    )
+}
+
+export default Footer;
